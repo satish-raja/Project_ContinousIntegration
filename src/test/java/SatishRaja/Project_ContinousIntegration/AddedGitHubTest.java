92c926c54ec74754b703652d5cb1bce1b7c87a4b
satish-raja <145887291+satish-raja@users.noreply.github.com>
2024-01-11 15:04:31 +0530

    Create AddedGitHubTest.java

---
+++ src/test/java/SatishRaja/Project_ContinousIntegration/AddedGitHubTest.java
@@ -0,0 +1,11 @@
+package SatishRaja.Project_ContinousIntegration;
+
+import org.testng.annotations.Test;
+
+public class AddedGitHubTest {
+	@Test
+	public void TestSample2() {
+		System.out.println("AddedGitHubTest");		
+	}
+
+}
